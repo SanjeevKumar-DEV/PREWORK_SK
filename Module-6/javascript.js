09eab23448e5c3e4f00b8235729a04957951fd29
Sanjeev Kumar <sanjeevkumar@me.com>
2021-11-16 17:47:14 +1100

    My first push for Prework assignmnet for Monash

---
+++ Module-6/javascript.js
@@ -0,0 +1,30 @@
+
+        function changeSize() {
+
+            document.getElementById("box").style.height = "300px";
+            document.getElementById("box").style.width = "300px";
+
+        }
+        function changeMargin() {
+
+            document.getElementById("box").style.margin = "50px";
+
+        }
+        function changeColor() {
+
+            document.getElementById("box").style.backgroundColor = "blue";
+
+        }
+        function changeFadeProperty() {
+
+            document.getElementById("box").style.opacity = "0.5";
+
+        }
+        function resetToOriginal() {
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.width = "150px";
+            document.getElementById("box").style.margin = "25px";
+            document.getElementById("box").style.backgroundColor = "orange";
+            document.getElementById("box").style.opacity = "1.0";
+
+        }
